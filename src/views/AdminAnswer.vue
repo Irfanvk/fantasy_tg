01489fcc3535be190af28901b4642418fc31bcde
irfanvk <ahamad.irfa@gmail.com>
2019-07-03 22:37:26 +0530

    admin answers part added

---
+++ src/views/AdminAnswer.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8"></base-header>
+
+    <!--Charts-->
+    <div class="container-fluid mt--7">
+      <!-- End charts-->
+
+      <!--Tables-->
+      <div class="row mt-2">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col">
+              <!-- <admin-answer-table title="Todays Questions"></admin-answer-table> -->
+              <el-table
+                :data="apiData"
+                style="border:2px solid black;border-radius:5px;max-width:420px"
+              >
+                <el-table-column type="index" />
+                <el-table-column prop="question" label="Question" width="150">
+                  <template slot-scope="scope">
+                    <!-- <el-button type="warning" size="small" circle icon="el-icon-edit"></el-button> -->
+                    <el-tag
+                      @click="$router.push('postadminanswer/'+scope.row.qid)"
+                    >{{scope.row.question}}</el-tag>
+                  </template>
+                </el-table-column>
+                <!-- <el-table-column prop="email" label="email" width="120"></el-table-column> -->
+              </el-table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--End tables-->
+    </div>
+  </div>
+</template>
+<script>
+// Tables
+// import PageVisitsTable from "./Dashboard/PageVisitsTable";
+// import ProjectsTable from "./Tables/ProjectsTable";
+import AdminanswerTable from "./Tables/AdminanswerTable";
+import { base_url } from "../../config";
+
+export default {
+  components: {
+    // PageVisitsTable,
+    // ProjectsTable,
+    AdminanswerTable
+  },
+  data() {
+    return {
+      apiData: []
+    };
+  },
+  methods: {
+    getQuestion() {
+      // console.log("ansques");
+      var url = base_url + "questions";
+
+      this.axios.get(url).then(response => {
+        console.log(response.data.questions);
+        console.log("aa");
+        this.apiData = response.data.questions;
+      });
+    }
+  },
+  created() {
+    this.getQuestion();
+  },
+  mounted() {},
+  beforeCreate() {
+    if (!localStorage.getItem("usertoken")) {
+      this.$router.push({ name: "login" });
+      // this.$router.go(-1);
+      this.$notify({
+        type: "danger",
+        message: "Please login to continue"
+      });
+    }
+  }
+};
+</script>
+<style></style>
---
+++ src/views/Tables/AdminanswerTable.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="card shadow" :class="type === 'dark' ? 'bg-default': ''">
+    <div class="card-header border-0" :class="type === 'dark' ? 'bg-transparent': ''">
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0" :class="type === 'dark' ? 'text-white': ''">{{title}}</h3>
+        </div>
+        <div class="col text-right">
+          <!-- <base-button type="primary" size="sm">See all</base-button> -->
+          <h6>click on the question ID to answer</h6>
+        </div>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <base-table
+        class="table align-items-center table-flush"
+        :class="type === 'dark' ? 'table-dark': ''"
+        :thead-classes="type === 'dark' ? 'thead-dark': 'thead-light'"
+        tbody-classes="list"
+        :data="quesData"
+      >
+        <template slot="columns">
+          <!-- <th>Index</th> -->
+          <th>Question ID</th>
+          <!-- <th>ID</th> -->
+        </template>
+
+        <template slot-scope="{row}">
+          <!-- <th scope="row" type="index" :data="qnum">{{qnum++}}</th> -->
+          <th scope="row" style="max-width:50%">
+            <div class="media align-items-center">
+              <div class="media-body">
+                <!-- <span class="name mb-0 text-sm" @click="open">{{row.question}}</span> -->
+
+                <el-tag
+                  type="primary"
+                  @click="$router.push('questions/'+row.question)"
+                >{{row.qid.toString().substr(0,18)}}</el-tag>
+              </div>
+            </div>
+          </th>
+          <!-- <td class="text-right">
+            <base-dropdown class="dropdown" position="right">
+              <a
+                slot="title"
+                class="btn btn-sm btn-icon-only text-light"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i class="fas fa-ellipsis-v"></i>
+              </a>
+
+              <!-- <template>
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+          </template>-->
+          <!-- </base-dropdown> -->
+          <!-- </td> -->
+        </template>
+      </base-table>
+    </div>
+  </div>
+</template>
+
+
+<!--Script-->
+<script>
+import { base_url } from "../../../config";
+export default {
+  name: "admin-answer-table",
+  props: {
+    type: {
+      type: String
+    },
+    title: String
+  },
+  data() {
+    return {
+      qnum: 1,
+      modals: { modal0: false },
+      sizeForm: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: [
+          {
+            option1: ""
+          }
+        ],
+        desc: ""
+      },
+      quesData: []
+    };
+  },
+  methods: {
+    //  .............................
+    open() {
+      this.$confirm("You have unsaved changes, save and proceed?", "Confirm", {
+        distinguishCancelAndClose: true,
+        confirmButtonText: "Save",
+        cancelButtonText: "Discard Changes"
+      })
+        .then(() => {
+          this.$message({
+            type: "info",
+            message: "Changes saved. Proceeding to a new route."
+          });
+        })
+        .catch(action => {
+          this.$message({
+            type: "info",
+            message:
+              action === "cancel"
+                ? "Changes discarded. Proceeding to a new route."
+                : "Stay in the current route"
+          });
+        });
+    },
+    onSubmit() {
+      console.log("submit");
+    },
+    // ............................
+    getQuestions() {
+      var options = {
+        day: "numeric",
+        weekday: "short",
+        year: "numeric",
+        month: "long"
+      };
+      var url = base_url + "questions";
+
+      // const token = localStorage.usertoken;
+      // var config = {
+      //   headers: { Authorization: "bearer " + token }
+      // };
+      this.axios.interceptors.request.use(
+        config => {
+          let token = localStorage.usertoken;
+
+          if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+          }
+
+          return config;
+        },
+
+        error => {
+          return Promise.reject(error);
+          this.axios.get;
+        }
+      );
+      this.axios.get(url).then(response => {
+        // console.log(response, "aa");
+        // console.log(response.data.questions);
+        this.quesData = response.data.questions;
+        // this.quesData = this.quesData.map(user => {
+        // console.log(Date.parse(user.added_on), "aaaaaaaaaaaaaaaaaaaa");
+        // user.added_on = new Date(Date.now()).toLocaleString("en", options);
+        // return user;
+        // });
+        // this.userCount = response.data.count;
+        console.log(this.quesData, "bb");
+      });
+    }
+  },
+  created() {
+    this.getQuestions();
+  }
+};
+</script>
+<style>
+.avatar {
+  background-color: #fff;
+}
+</style>
