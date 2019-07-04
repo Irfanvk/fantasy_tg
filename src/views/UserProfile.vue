776db1f50a5e71419e91d030e377d28438cf5945
irfanvk <ahamad.irfa@gmail.com>
2019-07-04 07:58:13 +0530

    profile modifications

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -2,7 +2,7 @@
   <div>
     <base-header
       class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-      style="min-height: 600px; background-image: url(img/theme/profile-cover.jpg); background-size: cover; background-position: center top;"
+      style="min-height: 600px; background-image: url(img/theme/team-1-800x800.jpg); background-size: cover; background-position: center top;"
     >
       <!-- Mask -->
       <span class="mask bg-gradient-success opacity-8"></span>
@@ -10,11 +10,11 @@
       <div class="container-fluid d-flex align-items-center">
         <div class="row">
           <div class="col-lg-7 col-md-10">
-            <h1 class="display-2 text-white">Hello Jesse</h1>
+            <h1 class="display-2 text-white">Hello {{full_name}}</h1>
             <p
               class="text-white mt-0 mb-5"
             >This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
-            <a href="#!" class="btn btn-info">Edit profile</a>
+            <!-- <a href="#!" class="btn btn-info">Edit profile</a> -->
           </div>
         </div>
       </div>
@@ -28,7 +28,7 @@
               <div class="col-lg-3 order-lg-2">
                 <div class="card-profile-image">
                   <a href="#">
-                    <img src="img/theme/team-4-800x800.jpg" class="rounded-circle">
+                    <img src="img/theme/team-1-800x800.jpg" class="rounded-circle" />
                   </a>
                 </div>
               </div>
@@ -58,29 +58,38 @@
                   </div>
                 </div>
               </div>
-              <div class="text-center">
+              <div class="text-center ni ni-glasses-2 mr-3 mt-6">
                 <h3>
-                  Jessica Jones
-                  <span class="font-weight-light">, 27</span>
+                  {{full_name}}
+                  <!-- <span class="font-weight-light">, 27</span> -->
                 </h3>
-                <div class="h5 font-weight-300">
+                <!-- <div class="h5 font-weight-300">
                   <i class="ni location_pin mr-2"></i>Bucharest, Romania
+                </div>-->
+                <div class="h5 mt-3">
+                  <i class="fa fa-mobile fa-lg text-blue mr-3" aria-hidden="true"></i>
+                  {{ mobile }}
                 </div>
-                <div class="h5 mt-4">
-                  <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+                <div class="h5 mt-3">
+                  <i class="ni ni-email-83 text-blue mr-3" aria-hidden="true"></i>
+                  {{ email }}
                 </div>
                 <div>
-                  <i class="ni education_hat mr-2"></i>University of Computer Science
+                  <i class="ni education_hat mr-1"></i>
+                  Team - {{team}}
                 </div>
-                <hr class="my-4">
+                <hr class="my-4" />
                 <p>Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
-                <a href="#">Show more</a>
+                <i href="#" v-if="admin">
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  '{{full_name}}' has admin privileges
+                </i>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="col-xl-8 order-xl-1">
+        <!-- <div class="col-xl-8 order-xl-1">
           <card shadow type="secondary">
             <div slot="header" class="bg-white border-0">
               <div class="row align-items-center">
@@ -91,8 +100,8 @@
                   <a href="#!" class="btn btn-sm btn-primary">Settings</a>
                 </div>
               </div>
-            </div>
-            <template>
+        </div>-->
+        <!-- <template>
               <form @submit.prevent>
                 <h6 class="heading-small text-muted mb-4">User information</h6>
                 <div class="pl-lg-4">
@@ -116,7 +125,7 @@
                       />
                     </div>
                   </div>
-                  <!-- <div class="row">
+                  <div class="row">
                     <div class="col-lg-6">
                       <base-input
                         alternative
@@ -135,11 +144,11 @@
                         v-model="model.lastName"
                       />
                     </div>
-                  </div>-->
+                  </div>
                 </div>
                 <hr class="my-4">
-                <!-- Address -->
-                <h6 class="heading-small text-muted mb-4">Contact information</h6>
+        <!-- Address-->
+        <!-- <h6 class="heading-small text-muted mb-4">Contact information</h6>
                 <div class="pl-lg-4">
                   <div class="row">
                     <div class="col-md-12">
@@ -183,8 +192,8 @@
                   </div>
                 </div>
                 <hr class="my-4">
-                <!-- Description -->
-                <!-- <h6 class="heading-small text-muted mb-4">About me</h6>
+        <!-- Description-->
+        <!-- <h6 class="heading-small text-muted mb-4">About me</h6>
                                 <div class="pl-lg-4">
                                     <div class="form-group">
                                         <base-input alternative=""
@@ -192,11 +201,11 @@
                                             <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
                                         </base-input>
                                     </div>
-                </div>-->
+                </div>
               </form>
-            </template>
-          </card>
-        </div>
+        </template>-->
+        <!-- </card>
+        </div>-->
       </div>
     </div>
   </div>
@@ -207,9 +216,8 @@ export default {
   name: "user-profile",
   data() {
     const token = localStorage.usertoken;
-    console.log("aaaaaa");
     const decoded = jwtDecode(token);
-    console.log(decoded);
+    // console.log(decoded.identity);
     return {
       full_name: decoded.identity.full_name,
       team: decoded.identity.team,
