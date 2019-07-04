bdddd731354604258912df3577969a06f2c51b8e
irfanvk <ahamad.irfa@gmail.com>
2019-07-04 08:02:56 +0530

    logout added

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="row justify-content-center">
-    <div class="col-lg-5 col-md-7"></div>
-    <div class="card bg-secondary shadow border-0">
-      <div class="card-body px-lg-5 py-lg-5">User logged out Successfully</div>
+    <div class="col-lg-5 col-md-7">
+      <div class="card bg-secondary shadow border-0">
+        <div class="card-body px-lg-5 py-lg-5 text-center">User logged out Successfully</div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +16,11 @@ export default {
       colors: ["#99A9BF", "#F7BA2A", "#FF9900"] // same as { 2: '#99A9BF', 4: { value: '#F7BA2A', excluded: true }, 5: '#FF9900' }
     };
   },
-
+  created() {
+    setTimeout(() => {
+      window.location = "/";
+    }, 5000);
+  },
   beforeCreate() {
     if (localStorage.getItem("usertoken")) {
       localStorage.clear();
