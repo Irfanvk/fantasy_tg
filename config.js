5bc3d477380a3b53690f1f07d8406776933e8c7c
irfanvk <ahamad.irfa@gmail.com>
2019-07-05 15:28:58 +0530

    production server updated

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-export const base_url = 'http://127.0.0.1:5000/'
+// local server
+// export const base_url = 'http://127.0.0.1:5000/'
+
+// production server url
+export const base_url = 'https://tg-backend-fantasy.herokuapp.com/'
