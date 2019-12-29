c5406a18aad6bfce014cdb2e385c562ab11b9e93
irfanvk <ahamad.irfa@gmail.com>
2019-12-29 16:09:32 +0530

    prod error  solve try

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -11,21 +11,24 @@
 
     <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
       <!-- </base-header> -->
-      <span v-for="data in matchData" v-bind:key="data.id">
+      <span v-for="data in matchData" v-bind:key="data.match_id">
         <base-button block type="default mt-3 " style="margin:0px 10px 0px 10px">
-          <small>{{data.srs}}</small>
+          <small>{{data.series_name}}</small>
           <br />
           <span>
             <strong>{{data.team1.name +" vs "+data.team2.name}}</strong>
           </span>
-          <badge type="black">{{data.id}}</badge>
+          <badge type="black">{{data.match_id}}</badge>
           <!-- <el-divider content-position="left">{{data.status}}</el-divider> -->
-          <el-divider content-position="left">{{data.status}}</el-divider>
-          <span>{{data.type}}</span>
+          <el-divider content-position="left">{{data.header.status}}</el-divider>
+          <span>{{data.header.type}}</span>
           <el-divider direction="vertical"></el-divider>
-          <span>{{data.mnum}}</span>
-          <el-divider direction="vertical"></el-divider>
-          <span>{{data.venue_location}}</span>
+          <span>{{data.header.match_desc}}</span>
+          <!-- <el-divider direction="vertical"></el-divider> -->
+          <!-- <el-divider></el-divider> -->
+          <br />
+          <small>{{data.venue.name}}</small>
+          <small>{{" ("+ data.venue.location}} )</small>
         </base-button>
       </span>
     </base-header>
@@ -35,7 +38,7 @@
 import { base_url } from "../../config";
 
 export default {
-  name: "matches",
+  name: "matches_",
   components: {},
   data() {
     return {
@@ -50,41 +53,52 @@ export default {
     },
     matchDetails() {
       this.loading = true;
-      var url = base_url + "getmatches";
+      // var url = base_url + "getmatches";
+      var url = base_url + "livematches";
       this.axios
         .get(url)
         .then(res => {
-          console.log(res.data);
-          this.matchData = res.data;
+          // console.log(res.data.matches);
+          // this.loading = false;
+
+          this.matchData = res.data.matches;
+          // console.log(this.matchData);
           this.loading = false;
         })
         .catch(err => {
+          this.loading = false;
           this.$notify({
             type: "warning",
-            message: err.data.msg
+            message: "something went wrong"
           });
         });
     }
   },
-  beforeCreate() {
+  created() {
+    var refreshdata = setInterval(this.matchDetails, 60000);
     // this.matchDetails();
-    this.loading = true;
-    this.value = 10;
-    var url = base_url + "getmatches";
-    this.axios
-      .get(url)
-      .then(res => {
-        console.log(res.data);
-        this.matchData = res.data;
-        this.loading = false;
-      })
-      .catch(err => {
-        this.$notify({
-          type: "warning",
-          message: err.data.msg
-        });
-      });
   }
+  // beforeCreate() {
+  //   // this.matchDetails();
+  //   this.loading = true;
+  //   // var url = base_url + "getmatches";
+  //   var url = "https://mapps.cricbuzz.com/cbzios/match/livematches";
+  //   // console.log(url);
+
+  //   this.axios
+  //     .get(url)
+  //     .then(res => {
+  //       console.log(url);
+  //       this.matchData = res.data.matches;
+  //       this.loading = false;
+  //     })
+  //     .catch(err => {
+  //       this.$notify({
+  //         type: "warning",
+  //         message: err
+  //       });
+  //     });
+  // }
 };
 </script>
 <style >
